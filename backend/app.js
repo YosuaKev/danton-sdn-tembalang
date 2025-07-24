0d18e524e5791d3bc41c8deb95f729f67e1616ec
DarisIlham <darisilham32@gmail.com>
2025-07-24 12:29:56 +0700

    menambahkan url pada cors

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: "https://sdn-tembalang-semarang.vercel.app",
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' })); 
