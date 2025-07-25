c11bc1e6837f99a71c68ce1d1ac3880b2b145c8a
Yosua <yosuakevan@gmail.com>
2025-07-25 08:37:35 +0700

    Perbaikan app.js

--- backend/app.js
+++ backend/app.js
@@ -23,9 +23,9 @@ const app = express();
 
 // CORS header fix (Vercel fix)
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://frontend-sdn-tembalang.vercel.app');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
