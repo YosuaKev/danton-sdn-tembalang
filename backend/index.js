38449e5494b1eb23845af78c8dc3ea1ea0980e23
Yosua <yosuakevan@gmail.com>
2025-07-25 10:57:09 +0700

    Perbaikan index.js 2

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 // index.js
 import app from './app.js';
 import serverless from 'serverless-http';
+import connectDB from "./config/db.js";
 
+await connectDB();
 export const handler = serverless(app);
