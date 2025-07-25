f3dd6a4875322c1fbd2ffee7a71969304f8d53bd
Yosua <yosuakevan@gmail.com>
2025-07-25 12:13:45 +0700

    fix vercel and index

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,5 @@
 // index.js
 import app from './app.js';
 import serverless from 'serverless-http';
-import connectDB from "./config/db.js";
 
-await connectDB();
 export const handler = serverless(app);
